3a7669e003a1dbb172a6eb021f77c9e67d2bf50d
Brian Hays <brian.hays@gmail.com>
2014-10-01 15:48:57 +0000

    Add initial seed data before implementing pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Place.create(name: "CrossFit Invoke", address: "1701 S Saunders St, Raleigh, NC 27603", description: "In a nutshell, it’s adult recess! Located adjacent to downtown Raleigh we Invoke Your Inner Athlete.")
+
+Place.create(name: "I AM CrossFit (Miami)", address: "1401 NW 88th Avenue Miami, FL 33172", description: "With the largest membership in South Florida, and it’s also the biggest box in the area at 20,000 square feet.")
+
+Place.create(name: "CrossFit Fenway", address: "100 Brookline Ave. Boston, MA 02215", description: "CrossFit Fenway was the first CrossFit facility to open in downtown Boston. This is classic, old-school CrossFit delivered in uniquely small class sizes with personal attention.")
+
+Place.create(name: "San Francisco CrossFit", address: "1162A Gorgas Avenue The Presidio of San Francisco. San Francisco, CA 94129", description: "Founded in 2005 and located under the Golden Gate Bridge, San Francisco CrossFit is one of the first CrossFit-affiliated gyms in the world. And when it comes to boxes, this one is literally a breath of fresh air, as it is a predominantly outdoor-based gym.")
+
+Place.create(name: "CrossFit NYC", address: "50 W 28th St., 2nd Floor New York, NY 10001", description: "CrossFit NYC is the largest gym in the nation and the oldest on the East Coast, with more than 1000 members and 200 classes a week between their two locations.")
+
+Place.create(name: "Windy City CrossFit", address: "4043 N. Ravenswood Ave. Chicago, IL 60613", description: "The first CrossFit facility in Chicago. Windy City CrossFit is a dedicated, full time strength and conditioning facility.")
+
+Place.create(name: "Northwest CrossFit", address: "13420 NE 16th St., Suite C Bellevue , WA 98005", description: "A collaborative and revolutionary community comprised of members who are interested in having a great time with the best fitness training available.")
+
+Place.create(name: "DogTown CrossFit", address: "2890 La Cienega Blvd. Culver City, CA 90232", description: "DogTown CrossFit is a strength and conditioning gym, run and owned by Adam Noble and Dusty Hyland... formerly coaches at CrossFit Culver City. We are NOT a dog agility facility, nor are we a gym for dogs.")
+
+Place.create(name: "NorCal CrossFit (HQ)", address: "2541 Seaboard Ave. San Jose, CA 95131", description: "Founded in 2008 by CrossFit Games Champion Jason Khalipa as CrossFit Santa Clara, NorCal CrossFit has quickly become the Bay Area’s largest and leading CrossFit affiliate.")
+
+Place.create(name: "CrossFit Mayhem", address: "41 South Cedar Ave, Suite A Cookeville, TN 38501", description: "Owned and operated by 4-time CrossFit Games Champion Rich Froning Jr. Enough said.")
