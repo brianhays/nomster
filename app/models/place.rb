c28e8021224b2ef52e20a0a5d64596b38996f336
Brian Hays <brian.hays@gmail.com>
2014-10-02 17:19:34 +0000

    Modify geocode rule to run on new or *updated* addresses, also changed map pin title to place name

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,7 @@ class Place < ActiveRecord::Base
 
 	belongs_to :user
 	geocoded_by :address
-	after_validation :geocode
+	after_validation :geocode, if: :address_changed?
 
 	validates :name, presence: true, length: {minimum: 3}
 	validates :address, presence: true
